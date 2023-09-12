eefbc06e595613d4052f911f8f0f8cefcf93a226
Felix Cary Giovanni <felix.cary@gmail.com>
2023-09-12 17:38:34 +0700

    add background color by type and fav icon

--- lib/views/pokemon_list/widgets/pokemon_list.dart
+++ lib/views/pokemon_list/widgets/pokemon_list.dart
@@ -20,9 +20,9 @@ class PokemonList extends StatelessWidget {
                 final List<String> parsedUrl =
                     StringUtil.getParsedUrl(pokemon['url']);
                 return Container(
-                  padding: const EdgeInsets.all(16),
                   decoration: ShapeDecoration(
-                    color: const Color(0xFFECF1F3),
+                    color: ColorUtil.getTypeColor(pokemonProvider.selectedType)
+                        .withOpacity(0.5),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
@@ -31,40 +31,72 @@ class PokemonList extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'No ${parsedUrl[1]}',
-                              style: const TextStyle(
-                                color: Color(0xFF333333),
-                                fontSize: 12,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w600,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'No ${parsedUrl[1]}',
+                                style: const TextStyle(
+                                  color: Color(0xFF333333),
+                                  fontSize: 12,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
-                            ),
-                            Text(
-                              StringUtil.capitalizeWords(pokemon['name']),
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 21,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w600,
+                              Text(
+                                StringUtil.capitalizeWords(pokemon['name']),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 21,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: CachedImage(
-                          imageUrl: StringUtil.getImageUrl(
-                            int.parse(
-                              parsedUrl[1],
-                            ),
+                            ],
                           ),
                         ),
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: ShapeDecoration(
+                              color: ColorUtil.getTypeColor(
+                                  pokemonProvider.selectedType),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            height: 120,
+                            width: 120,
+                            child: CachedImage(
+                              imageUrl: StringUtil.getImageUrl(
+                                int.parse(parsedUrl[1]),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: 8,
+                            top: 8,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              padding: const EdgeInsets.all(4),
+                              child: const Icon(
+                                Icons.favorite_border,
+                                size: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       )
                     ],
                   ),
